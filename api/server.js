38b29fc24e368719045c0c3338378284adcdb978
AlexAndrew Mallory <pytormal@live.com>
2021-05-16 21:23:53 -0700

    added to server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json());
+
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.use((err, req, res, next) => {
+  server.get("/", (req, res) => {
+    res.json({ api: "up" });
+  });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 9500;
+
+server.listen(port, () =>
+  console.log(`\n ** server on https://localhost:${port} ** \n`)
+);
